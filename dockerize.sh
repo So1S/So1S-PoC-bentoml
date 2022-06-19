6f5ed9265b8551dbff278029a352518dace90f54
jiho lee <optional.int@kakao.com>
2022-06-19 15:25:01 +0900

    fix: remove docker run

--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,4 @@
 # uses python <= 3.8
 pip install -r requirements.txt
 python ./src/main.py  # bentoml bundle 만들기
-bentoml containerize So1sService:latest -t tf_service:v1  # dockerize
-docker run -p 5000:5000 tf_service:v1  # 서빙
+bentoml containerize So1sService:latest -t So1sService:latest  # dockerize
