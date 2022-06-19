229740c00f41f5eb6089b8de4a4933d97bbfaf6b
Jiho Lee <optional.int@kakao.com>
2022-06-19 22:49:36 +0900

    fix: delete verbose

--- dockerize.sh
+++ dockerize.sh
@@ -4,4 +4,4 @@ export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 # uses python <= 3.8
 pip install -r requirements.txt
 python ./src/main.py  # bentoml bundle 만들기
-bentoml --verbose containerize So1sService:latest -t so1sservice:latest  # dockerize
+bentoml containerize So1sService:latest -t so1sservice:latest  # dockerize
