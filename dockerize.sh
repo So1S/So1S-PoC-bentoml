8a0379e79384e1aed40c2ae5f881034158574d20
jiho lee <optional.int@kakao.com>
2022-06-19 14:43:36 +0900

    feat: install requirements.txt

--- dockerize.sh
+++ dockerize.sh
@@ -1,3 +1,5 @@
+# uses python <= 3.8
+pip install -r requirements.txt
 python ./src/main.py  # bentoml bundle 만들기
 bentoml containerize So1sService:latest -t tf_service:v1  # dockerize
 docker run -p 5000:5000 tf_service:v1  # 서빙
